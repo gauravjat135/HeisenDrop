bb24c700bc2f3624e30121e77b57b91cfe7aba98
gauravjat135 <gauraviskgf@gmail.com>
2025-07-14 20:20:15 +0530

    Add files via upload

---
+++ data2.js
@@ -0,0 +1,108 @@
+const allData = [
+
+    //Movies part
+
+    {
+        title: "Kesari Chapter 2 The Untold Story of Jallianwala Bagh",
+        year: "2025",
+        rating: "7.5",
+        type: "Movie",
+        quality: "HD (720p)",
+        genre: "Action, History, Drama",
+        cast: "Akshay Kumar, Annanya Panday",
+        plot: "A gripping tale of the Jallianwala Bagh massacre, exploring the events leading up to the tragic incident and its aftermath.",
+        poster: "https://m.media-amazon.com/images/M/MV5BOTJiYTA4NDItMWNiYy00YmE3LTg4ZGItNjVlOGZlZGY4MjExXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+        download_url: "https://new20.gdtot.dad/file/1287138742"
+    },
+
+    {
+        title: "Chhaava",
+        year: "2025",
+        rating: "8.5",
+        type: "Movie",
+        quality: "HD (720p)",
+        genre: "Action, History, Drama",
+        cast: "Rashmika Mandanna, Vicky Kaushal, Vineet Kumar Singh",
+        plot: "A biopic of Maratha warrior Chhatrapati Sambhaji Maharaj.",
+        poster: "https://m.media-amazon.com/images/I/61-lNA-rD5L._UF1000,1000_QL80_.jpg",
+        download_url: "https://new20.gdtot.dad/file/1119164905"
+    },
+
+    {
+        id: 1,
+        title: "Padmaavat",
+        year: 2018,
+        rating: "7.7/10",
+        genre: "Biography, Drama, History",
+        quality: "HD (720p)",
+        poster: "https://image.tmdb.org/t/p/w500/dtIceHDioZP4oHAmhHlWmDuZEM9.jpg",
+        plot: "Set in medieval Rajasthan, Queen Padmavati is married to Maharawal Ratan Singh. The kingdom is invaded by Alauddin Khilji, who becomes obsessed with Padmavati's beauty.",
+        director: "Sanjay Leela Bhansali",
+        cast: "Deepika Padukone, Shahid Kapoor, Ranveer Singh",
+        runtime: "164 min",
+        detail: "must watch",
+        type: "Movie",
+        download_url: "https://drive.usercontent.google.com/download?id=1tFaFdCtY1K_JcvcGJwxesYyuTYfdGcl3&export=download&authuser=0"
+    },
+    {
+        title: "Inception",
+        year: "2010",
+        rating: "8.8",
+        type: "Movie",
+        quality: "HD (720p)",
+        detail: "must watch",
+        genre: "Action, Sci-Fi",
+        cast: "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page",
+        plot: "A thief who uses dream-sharing tech is given the inverse task of planting an idea.",
+        poster: "https://image.tmdb.org/t/p/w500/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg",
+        download_url: "https://drive.usercontent.google.com/download?id=1yof0tCINz37iEmKb7EQak9p1FRaLJJjl&export=download&authuser=0"
+    },
+    {
+        id: 3,
+        title: "The Martian",
+        year: 2015,
+        rating: "8.9/10",
+        detail: "must watch",
+        quality: "HD (720p)",
+        genre: "sci-fi, Drama",
+        poster: "https://upload.wikimedia.org/wikipedia/en/c/cd/The_Martian_film_poster.jpg",
+        plot: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+        director: "Quentin Tarantino",
+        cast: "John Travolta, Uma Thurman, Samuel L. Jackson",
+        runtime: "154 min",
+        language: "English",
+        type: "Movie",
+        download_url: "https://drive.usercontent.google.com/download?id=1cU4gl6GvHJllEmPW7miyipe_8ULnOPjU&export=download&authuser=0"
+    },
+    {
+        title: "Mukkabaaz",
+        year: "2018",
+        rating: "7.8",
+        type: "Movie",
+        quality: "HD (720p)",
+        detail: "must watch",
+        genre: "Action, Drama, Sport",
+        cast: "Vineet Kumar Singh, Zoya Hussain, Ravi Kishan",
+        plot: "A low caste boxer trains at a gym controlled by a local Don. Shravan falls for Mishra’s niece. The Don does not approve of this match. Shravan strives to win Sunaina’s hand in marriage and become a successful boxer while trying to avoid retaliation from the Don.",
+        poster: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/mukkabaaz-et00059419-07-07-2017-05-57-42.jpg",
+        download_url: "https://drive.usercontent.google.com/download?id=1W3fDQXUREvs6-zdywBzEwlrfdL9IOB-E&export=download&authuser=0"
+    },
+
+    {
+        title: "Adipurush",
+        year: "2023",
+        rating: "7.8",
+        type: "Movie",
+        quality: "HD (720p)",
+        genre: "Action, Adventure, Drama",
+        cast: "Prabhas, Kriti Sanon, Saif Ali Khan",
+        plot: "A mythological epic based on the Ramayana, depicting the battle between Lord Rama and Ravana.",
+        poster: "https://filmfare.wwmindia.com/content/2022/sep/adipurush-poster-first-look.jpg",
+        download_url: "https://drive.usercontent.google.com/download?id=10LDY_CgQ59y4lbgT3-a_vzjTmRcMYYf5&export=download&authuser=0"
+    },
+
+
+
+
+
+]

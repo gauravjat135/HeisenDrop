7da94ae82709ff5fdd011155954d084f885663f0
gauravjat135 <gauraviskgf@gmail.com>
2025-07-18 11:58:56 +0530

    data.js

--- data.js
+++ data.js
@@ -227,7 +227,7 @@ const allData = [
         cast: "Akshay Kumar, Riteish Deshmukh, jacqueline Fernandez, nana Patekar",
         plot: "Housefull 5 has been released in 2 versions, Housefull 5A and 5B. Both have different climaxes, different killers and different endings. The first two hours are the same in both versions, but the last 20 minutes differ.",
         poster: "https://images.fandango.com/ImageRenderer/0/0/redesign/static/img/default_poster--dark-mode.png/0/images/masterrepository/Fandango/240756/housefull5-9x16_Overseas.jpg",
-        download_url: "https://new20.gdtot.dad/file/1517860235"
+        download_url: "https://new20.gdtot.dad/file/1531152636"
     },
     {
         title: "Housefull 5B",
@@ -239,7 +239,7 @@ const allData = [
         cast: "Sinbad, Taraji P. Henson, Teyana Taylor",
         plot: " Housefull 5 has been released in 2 versions, Housefull 5A and 5B. Both have different climaxes, different killers and different endings. The first two hours are the same in both versions, but the last 20 minutes differ.",
         poster: "https://images.fandango.com/ImageRenderer/0/0/redesign/static/img/default_poster--dark-mode.png/0/images/masterrepository/Fandango/240756/housefull5-9x16_Overseas.jpg",
-        download_url: "https://new20.gdtot.dad/file/1518789646"
+        download_url: "https://new20.gdtot.dad/file/1530483604"
     },
     {
         title: "KO",
@@ -253,7 +253,171 @@ const allData = [
         poster: "https://m.media-amazon.com/images/M/MV5BZTExNjM4NzAtY2FiZS00M2VmLWE1MjUtNWM1MTk3YTg1ZjQwXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
         download_url: "https://new20.gdtot.dad/file/967641071"
     },
-    
+
+
+
+    {
+        title: "Karate Kid Legends",
+        year: "2025",
+        rating: "7.5",
+        type: "Movie",
+        quality: "HD (720p)",
+        genre: "Action, Adventure, Drama",
+        cast: "Jackie Chan",
+        plot: "After a family tragedy, kung fu prodigy Li Fong is uprooted from his home in Beijing and forced to move to New York City with his mother.",
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPT2qMHZs0ZbvLtHI70Z4l3DMhQbVdT9VwzYHDieNmsYnk_YX_B5xS_sqU&s=10",
+        download_url: "https://new20.gdtot.dad/file/1048127467"
+    },
+
+    {
+        title: "Straw",
+        year: "2025",
+        rating: "6.5",
+        type: "Movie",
+        quality: "Full HD (1080p)",
+        genre: "Comedy, Family, Romance, Drama",
+        cast: "Akshay Kumar, Riteish Deshmukh, jacqueline Fernandez, nana Patekar",
+        plot: "A single mother navigates a series of unfortunate events, leading her down an unforeseen path.",
+        poster: "https://images.plex.tv/photo?size=large-1280&url=https%3A%2F%2Fmetadata-static.plex.tv%2F2%2Fgracenote%2F20a067df3a2ba64975d4f73e5fa6d757.jpg",
+        download_url: "https://new20.gdtot.dad/file/2387493585"
+    },
+
+    {
+        title: "Kesari Veer",
+        year: "2025",
+        rating: "7.1",
+        type: "Movie",
+        quality: "HD (720p)",
+        genre: " Drama, History",
+        cast: "Sooraj Pancholi, Suniel Shetty, Vivek Oberoi",
+        plot: "The movie narrates the tale of Hamirji Gohil, a brave warrior who fought against the Tughlaq Empire to protect the Somnath Temple and the Hindu faith.",
+        poster: "https://m.media-amazon.com/images/M/MV5BZDU2MjhkMmYtMzc4Ni00Y2VmLTg3YjMtYTljNzViOWJhNzQ0XkEyXkFqcGc@._V1_.jpg",
+        download_url: "https://new20.gdtot.dad/file/1476779065"
+    },
+
+        {
+        title: "Raid 2",
+        year: "2025",
+        rating: "7.5",
+        type: "Movie",
+        quality: "HD (720p)",
+        genre: "Action, Crime, Drama",
+        cast: "Ajay Devgn, Ritesh Deshmukh, Vaani Kapoor",
+        plot: "After exposing corruption, the protagonist must go deeper into the underworld.",
+        poster: "https://tse3.mm.bing.net/th/id/OIP.u1WlBRRAiYdCuii6NgCtsgHaE8?pid=Api",
+        download_url: "https://new20.gdtot.dad/file/1333972866"
+    },
+
+        {
+        title: "Mission Impossible The Final Reckoning",
+        year: "2025",
+        rating: "8.5",
+        type: "Movie",
+        quality: "CAMRip",
+        genre: "Action, Adventure, Thriller",
+        cast: "Tom Cruise, Rebecca Ferguson, Simon Pegg",
+        plot: "Ethan Hunt and his team embark on their most dangerous mission yet, facing a global conspiracy that threatens the world.",
+        poster: "https://m.media-amazon.com/images/M/MV5BZGQ5NGEyYTItMjNiMi00Y2EwLTkzOWItMjc5YjJiMjMyNTI0XkEyXkFqcGc@._V1_.jpg",
+        download_url: "https://new20.gdtot.dad/file/1830848499"
+    },
+        {
+        title: "Final Destination Bloodlines",
+        year: "2025",
+        rating: "7.1",
+        type: "Movie",
+        quality: "HD (720p)",
+        genre: "Horror, Mystery",
+        cast: "Richard Harmon, Rya Kihlstedt, Teo Briones",
+        plot: "Plagued by a violent recurring nightmare, college student Stefanie heads home to track down the one person.",
+        poster: "https://m.media-amazon.com/images/M/MV5BYWI0ZjgwOWItNDc0Mi00MmMxLWIyMGMtZTlhMTc1ZDg2ZDM2XkEyXkFqcGc@._V1_.jpg",
+        download_url: "https://new20.gdtot.dad/file/939601470"
+    },
+    {
+        title: "Kesari Chapter 2 The Untold Story of Jallianwala Bagh",
+        year: "2025",
+        rating: "7.5",
+        type: "Movie",
+        quality: "HD (720p)",
+        genre: "Action, History, Drama",
+        cast: "Akshay Kumar, Annanya Panday",
+        plot: "A gripping tale of the Jallianwala Bagh massacre, exploring the events leading up to the tragic incident and its aftermath.",
+        poster: "https://m.media-amazon.com/images/M/MV5BOTJiYTA4NDItMWNiYy00YmE3LTg4ZGItNjVlOGZlZGY4MjExXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+        download_url: "https://new20.gdtot.dad/file/1287138742"
+    },
+
+    {
+        title: "Chhaava",
+        year: "2025",
+        rating: "8.5",
+        type: "Movie",
+        quality: "HD (720p)",
+        genre: "Action, History, Drama",
+        cast: "Rashmika Mandanna, Vicky Kaushal, Vineet Kumar Singh",
+        plot: "A biopic of Maratha warrior Chhatrapati Sambhaji Maharaj.",
+        poster: "https://m.media-amazon.com/images/I/61-lNA-rD5L._UF1000,1000_QL80_.jpg",
+        download_url: "https://new20.gdtot.dad/file/1119164905"
+    },
+
+    {
+        id: 1,
+        title: "Padmaavat",
+        year: 2018,
+        rating: "7.7/10",
+        genre: "Biography, Drama, History",
+        quality: "HD (720p)",
+        poster: "https://image.tmdb.org/t/p/w500/dtIceHDioZP4oHAmhHlWmDuZEM9.jpg",
+        plot: "Set in medieval Rajasthan, Queen Padmavati is married to Maharawal Ratan Singh. The kingdom is invaded by Alauddin Khilji, who becomes obsessed with Padmavati's beauty.",
+        director: "Sanjay Leela Bhansali",
+        cast: "Deepika Padukone, Shahid Kapoor, Ranveer Singh",
+        runtime: "164 min",
+        detail: "must watch",
+        type: "Movie",
+        download_url: "https://new20.gdtot.dad/file/3047640833"
+    },
+    {
+        title: "Inception",
+        year: "2010",
+        rating: "8.8",
+        type: "Movie",
+        quality: "HD (720p)",
+        detail: "must watch",
+        genre: "Action, Sci-Fi",
+        cast: "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page",
+        plot: "A thief who uses dream-sharing tech is given the inverse task of planting an idea.",
+        poster: "https://image.tmdb.org/t/p/w500/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg",
+        download_url: "https://new20.gdtot.dad/file/1649989660"
+    },
+    {
+        id: 3,
+        title: "The Martian",
+        year: 2015,
+        rating: "8.9/10",
+        detail: "must watch",
+        quality: "HD (720p)",
+        genre: "sci-fi, Drama",
+        poster: "https://upload.wikimedia.org/wikipedia/en/c/cd/The_Martian_film_poster.jpg",
+        plot: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+        director: "Quentin Tarantino",
+        cast: "John Travolta, Uma Thurman, Samuel L. Jackson",
+        runtime: "154 min",
+        language: "English",
+        type: "Movie",
+        download_url: "https://new20.gdtot.dad/file/1686263584"
+    },
+    {
+        title: "Mukkabaaz",
+        year: "2018",
+        rating: "7.8",
+        type: "Movie",
+        quality: "HD (720p)",
+        detail: "must watch",
+        genre: "Action, Drama, Sport",
+        cast: "Vineet Kumar Singh, Zoya Hussain, Ravi Kishan",
+        plot: "A low caste boxer trains at a gym controlled by a local Don. Shravan falls for Mishra’s niece. The Don does not approve of this match. Shravan strives to win Sunaina’s hand in marriage and become a successful boxer while trying to avoid retaliation from the Don.",
+        poster: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/mukkabaaz-et00059419-07-07-2017-05-57-42.jpg",
+        download_url: "https://new20.gdtot.dad/file/1507652994"
+    },
+
 
 
 
